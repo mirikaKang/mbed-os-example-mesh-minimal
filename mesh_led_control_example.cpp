12321ace65b387bf4a08d0f3cdf75d132d79b368
mirikaKang <mirika.kang@sigma-delta.tech>
2019-08-20 14:02:07 +0900

    modify example for NUCLEO_F429ZI

--- mesh_led_control_example.cpp
+++ mesh_led_control_example.cpp
@@ -38,7 +38,9 @@ static void handle_message(char* msg);
 
 DigitalOut led_1(MBED_CONF_APP_LED, 1);
 InterruptIn my_button(MBED_CONF_APP_BUTTON);
-DigitalOut output(MBED_CONF_APP_RELAY_CONTROL, 1);
+#if !(MBED_CONF_APP_RELAY_CONTROL==NC)
+	DigitalOut output(MBED_CONF_APP_RELAY_CONTROL, 1);
+#endif 
 
 NetworkInterface * network_if;
 UDPSocket* my_socket;
@@ -110,13 +112,17 @@ static void update_state(uint8_t state) {
        tr_debug("Turning led on\n");
        led_1 = 0;
        button_status=1;
-       output = 0;
+	   #if !(MBED_CONF_APP_RELAY_CONTROL==NC)
+       	output = 0;
+	   #endif
        }
     else {
        tr_debug("Turning led off\n");
        led_1 = 1;
        button_status=0;
-       output = 1;
+	   #if !(MBED_CONF_APP_RELAY_CONTROL==NC)
+	   	output = 1;
+	   #endif
    }
 }
 
